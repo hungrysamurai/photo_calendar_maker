dbc2f7a85ad8540581eddb00b7b17ae465cc9aea
hungry samurai <kryjeka@mail.ru>
2023-03-11 17:55:19 +0300

    cropper integration

--- photo_calendar_maker/script.js
+++ photo_calendar_maker/script.js
@@ -1,15 +1,24 @@
 import glyphs from "./assets/Montserrat/MontserratGlyphs.js";
+import Cropper from "./assets/cropperjs/cropper.esm.js";
 
 PDFDocument.prototype.addSVG = function (svg, x, y, options, lang) {
   return SVGtoPDF(this, svg, x, y, options), this;
 };
 
 class MultiPageCalendar {
-  constructor(firstMonthIndex, year, parentContainer, controlsContainer, lang) {
+  constructor(
+    firstMonthIndex,
+    year,
+    parentContainer,
+    controlsContainer,
+    cropControlsContainer,
+    lang
+  ) {
     this.firstMonthIndex = firstMonthIndex;
     this.year = year;
     this.parentContainer = parentContainer;
     this.controlsContainer = controlsContainer;
+    this.cropControlsContainer = cropControlsContainer;
     this.lang = lang;
 
     // Dimensions of document (px)
@@ -36,6 +45,7 @@ class MultiPageCalendar {
     this.initDOMSVG();
     this.initControls();
     this.initControlsEvents();
+    // this.initCropperControls();
 
     this.pagesArray = [...this.calendarInner.querySelectorAll("svg")];
   }
@@ -81,9 +91,6 @@ class MultiPageCalendar {
           </g>
 
           <g id="days-titles-${i}">
-
-
-
           </g>
 
         </g>
@@ -236,7 +243,9 @@ class MultiPageCalendar {
           <option value="A3">A3</option>
        </select>
 
-
+    <button id="crop-btn">
+          <img src='./photo_calendar_maker/assets/icons/crop.svg'/>
+    </button>
 
     <input
       type="file"
@@ -265,11 +274,16 @@ class MultiPageCalendar {
     this.formatSelectBtn =
       this.controlsContainer.querySelector("#format-select");
 
+    this.cropBtn = this.controlsContainer.querySelector("#crop-btn");
     this.uploadImgInput = this.controlsContainer.querySelector("#upload-input");
   }
 
   initControlsEvents() {
     this.nextBtn.addEventListener("click", () => {
+      if (this.cropper) {
+        this.removeCropper();
+      }
+
       this.currentMonth++;
       if (this.currentMonth > 11) {
         this.currentMonth = 0;
@@ -279,6 +293,10 @@ class MultiPageCalendar {
     });
 
     this.prevBtn.addEventListener("click", () => {
+      if (this.cropper) {
+        this.removeCropper();
+      }
+
       this.currentMonth--;
       if (this.currentMonth < 0) {
         this.currentMonth = 11;
@@ -288,20 +306,51 @@ class MultiPageCalendar {
     });
 
     this.currentPDFDownloadBtn.addEventListener("click", () => {
+      if (this.cropper) {
+        this.removeCropper();
+      }
+
       this.downloadPDF("current");
     });
 
     this.allPDFDownloadBtn.addEventListener("click", () => {
+      if (this.cropper) {
+        this.removeCropper();
+      }
+
       this.downloadPDF("all");
     });
 
     this.pngDownloadBtn.addEventListener("click", () => {
+      if (this.cropper) {
+        this.removeCropper();
+      }
+
       this.downloadCurrentMonthPNG();
     });
 
+    this.cropBtn.addEventListener("click", () => {
+      if (this.cropper) return;
+
+      const currentImageElement = this.calendarInner.querySelector(
+        `#month-${this.currentMonth}-container image`
+      );
+
+      if (currentImageElement) {
+        // this.initCropperControls();
+        this.initCropper(currentImageElement);
+        this.cropControlsContainer.classList.remove("hide");
+      }
+    });
+
     this.uploadImgInput.addEventListener("input", (e) => {
+      if (this.cropper) {
+        this.removeCropper();
+      }
+
       this.uploadImgCurrentMonth(e);
     });
+
     this.formatSelectBtn.addEventListener("input", (e) => {
       this.currentSize = e.target.value;
     });
@@ -418,7 +467,10 @@ class MultiPageCalendar {
   uploadImgCurrentMonth(e) {
     if (!e.target.files[0]) return;
 
+    const imageFile = e.target.files[0];
+
     const reader = new FileReader();
+
     reader.onload = (e) => {
       const imageGroup = this.calendarInner.querySelector(
         `#image-group-${this.currentMonth}`
@@ -441,27 +493,136 @@ class MultiPageCalendar {
         "href",
         e.target.result
       );
-      // imageEl.setAttribute('clip-path', "url(#image-clip)");
 
       imageGroup.appendChild(imageEl);
       console.log("...image uploaded!");
 
-      // console.log(getComputedStyle(imageEl).transform);
-      // console.log(imageEl.getBoundingClientRect());
-
-      // Here to mess with cropper
-      // const temp = document.createElement("div");
-      // temp.style.position = "absolute";
-      // temp.style.left = `${imageEl.getBoundingClientRect().left}px`;
-      // temp.style.top = `${imageEl.getBoundingClientRect().top}px`;
-      // temp.style.width = `${imageEl.getBoundingClientRect().width}px`;
-      // temp.style.height = `${imageEl.getBoundingClientRect().height}px`;
-
-      // document.body.append(temp);
+      // this.initCropper(imageEl);
     };
 
-    reader.readAsDataURL(e.target.files[0]);
+    reader.readAsDataURL(imageFile);
     console.log("start upload image...");
+  }
+
+  initCropper(currentImageElement) {
+    const imageFile = currentImageElement.getAttribute("href");
+
+    fetch(imageFile)
+      .then((res) => res.blob())
+      .then((imageFile) => {
+        this.cropperOuter = document.createElement("div");
+        this.cropperOuter.classList.add("cropper-outer-container");
+
+        this.cropperOuter.style.position = "absolute";
+        this.cropperOuter.style.left = `${
+          currentImageElement.getBoundingClientRect().left
+        }px`;
+        this.cropperOuter.style.top = `${
+          currentImageElement.getBoundingClientRect().top
+        }px`;
+        this.cropperOuter.style.width = `${
+          currentImageElement.getBoundingClientRect().width
+        }px`;
+        this.cropperOuter.style.height = `${
+          currentImageElement.getBoundingClientRect().height
+        }px`;
+
+        const imageElement = document.createElement("img");
+        imageElement.classList.add("image-element");
+        imageElement.src = URL.createObjectURL(imageFile);
+
+        this.cropperOuter.appendChild(imageElement);
+
+        document.body.append(this.cropperOuter);
+        currentImageElement.style.display = "none";
+
+        this.cropper = new Cropper(imageElement, {
+          viewMode: 2,
+          dragMode: "move",
+          modal: false,
+          background: false,
+          autoCropArea: 1,
+
+          ready: () => {
+            // const bindedApply = this.applyCrop.bind(this);
+            // this.bindedApplyCropFunction = bindedApply;
+
+            this.initCropperControls();
+
+            this.applyCropBtn.addEventListener("click", () => {
+              this.applyCrop(currentImageElement);
+            });
+          },
+
+          zoom: (e) => {
+            if (e.detail.ratio < e.detail.oldRatio) {
+              if (
+                this.cropper.canvasData.width - 100 <
+                this.cropper.initialCanvasData.width
+              ) {
+                this.cropper.reset();
+              }
+            }
+          },
+        });
+      });
+  }
+
+  applyCrop(currentImageElement) {
+    // const currentImageElement = this.calendarInner.querySelector(
+    //   `#month-${this.currentMonth}-container image`
+    // );
+    // console.log(this.cropper);
+    const canvas = this.cropper.getCroppedCanvas();
+
+    const ctx = canvas.getContext("2d", {
+      willReadFrequently: true,
+    });
+
+    ctx.drawImage(canvas, 0, 0);
+    const resultURL = canvas.toDataURL("image/png");
+
+    currentImageElement.setAttributeNS(
+      "http://www.w3.org/1999/xlink",
+      "href",
+      resultURL
+    );
+
+    currentImageElement.style.display = "block";
+    this.removeCropper();
+  }
+
+  removeCropper() {
+    const currentImageElement = this.calendarInner.querySelector(
+      `#month-${this.currentMonth}-container image`
+    );
+
+    currentImageElement.style.display = "block";
+    this.cropper.destroy();
+    this.cropper = undefined;
+    this.cropperOuter.remove();
+    this.cropperOuter = undefined;
+
+    this.cropControlsContainer.innerHTML = "";
+  }
+
+  initCropperControls() {
+    this.cropControlsContainer.innerHTML = `
+    <button id="apply-crop">
+    <img src='./photo_calendar_maker/assets/icons/done.svg'/>
+    </button>
+    <button id="cancel-crop">
+    <img src='./photo_calendar_maker/assets/icons/cancel.svg'/>
+    </button>
+    `;
+
+    this.applyCropBtn = this.cropControlsContainer.querySelector("#apply-crop");
+    this.cancelCropBtn =
+      this.cropControlsContainer.querySelector("#cancel-crop");
+
+    this.cancelCropBtn.addEventListener("click", () => {
+      this.removeCropper();
+    });
   }
 
   createMonthGrid(monthGrid, startIndex, totalDays, prevMonthDaysNumber) {
@@ -549,6 +710,9 @@ const yearInput = document.querySelector("#year-input");
 
 const calendarContainer = document.querySelector(".calendar-container");
 const controlsContainer = document.querySelector(".controls-container");
+const cropControlsContainer = document.querySelector(
+  ".crop-controls-container"
+);
 
 let currentCalendar;
 getButton.addEventListener("click", () => {
@@ -556,12 +720,15 @@ getButton.addEventListener("click", () => {
   const month = +monthInput.value;
 
   calendarContainer.innerHTML = "";
-
+  if (document.querySelector(".cropper-outer-container")) {
+    document.querySelector(".cropper-outer-container").remove();
+  }
   currentCalendar = new MultiPageCalendar(
     month,
     year,
     calendarContainer,
     controlsContainer,
+    cropControlsContainer,
     "ru"
   );
 });
