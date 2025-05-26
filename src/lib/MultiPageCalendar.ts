870482a294a21be2ab93a7950f53b1ba087ca9b6
hungry samurai <hello@hungrysamurai.ru>
2025-05-26 21:32:38 +0300

    setVisibleMonth transform instead left

--- src/lib/MultiPageCalendar.ts
+++ src/lib/MultiPageCalendar.ts
@@ -306,9 +306,10 @@ export class MultiPageCalendar extends Calendar {
             ),
           ],
           attributes: {
-            transform: `translate(${this.mockupOptions.calendarGridX +
+            transform: `translate(${
+              this.mockupOptions.calendarGridX +
               this.mockupOptions.dayCellWidth * i
-              } ${this.mockupOptions.weekDaysY})`,
+            } ${this.mockupOptions.weekDaysY})`,
           },
         });
       });
@@ -394,8 +395,7 @@ export class MultiPageCalendar extends Calendar {
    * @property {Function} setVisibleMonth - show current month mockup in DOM by translate calendarInner container by X axis
    */
   static setVisibleMonth(): void {
-    this.current.calendarInner.style.left = `-${this.current.currentMonth * 100
-      }%`;
+    this.current.calendarInner.style.transform = `translateX(calc(-8.333333% * ${this.current.currentMonth}))`;
   }
 
   /**
@@ -438,9 +438,9 @@ export class MultiPageCalendar extends Calendar {
           const reduced = await Calendar.reduceImageSize(
             reader.result as string,
             this.current.mockupOptions.imagePlaceholderWidth *
-            this.current.imageReduceSizeRate,
+              this.current.imageReduceSizeRate,
             this.current.mockupOptions.imagePlaceholderHeight *
-            this.current.imageReduceSizeRate
+              this.current.imageReduceSizeRate
           );
 
           const resultImage = reduced ? reduced : reader.result;
